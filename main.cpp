b9502b4368a7158797ff4af778085e4bda726c0b
Pavel Lengarov <pavel.lengarov@circles.bz>
2016-08-03 11:34:21 +0300

    Some code is added in main.c

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+int test1()
+{
+    // exit normally
+    return 0;
+}
+
 int main()
 {
+    int a   = 0;
+    int res = -1;
 
-    return 1;
+    // some code here
+    res = test1();
+
+
+    // some other code
+
+
+    // program exit normally
+    return 0;
 }
